0ef30e6ed5d30ac58840cd5ddbf325fdde78c632
joseluizduarte <jlduartejr@live.com>
2022-01-27 00:33:13 -0400

    Implementação do redirecionamento ao WhatsApp

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -67,3 +67,31 @@ function alterarBotaoDoFooter () {
         botaoDoFooter.removeAttribute("disabled");
     }
 }
+
+// função para fechar o pedido
+function fecharPedido () {
+    let prato = document.getElementById(`prato${pratoSelecionado}`);
+    let pratoNome = prato.getElementsByClassName("card__title")[0].innerText;
+    let pratoPreco = prato.getElementsByClassName("card__lastline__price")[0].innerText;
+    pratoPreco = pratoPreco.slice(3);
+    pratoPreco = pratoPreco.replace(",",".");
+    pratoPreco = parseFloat(pratoPreco);
+    let bebida = document.getElementById(`bebida${bebidaSelecionada}`);
+    let bebidaNome = bebida.getElementsByClassName("card__title")[0].innerText;
+    let bebidaPreco = bebida.getElementsByClassName("card__lastline__price")[0].innerText;
+    bebidaPreco = bebidaPreco.slice(3);
+    bebidaPreco = bebidaPreco.replace(",",".");
+    bebidaPreco = parseFloat(bebidaPreco);
+    let sobremesa = document.getElementById(`sobremesa${sobremesaSelecionada}`);
+    let sobremesaNome = sobremesa.getElementsByClassName("card__title")[0].innerText;
+    let sobremesaPreco = sobremesa.getElementsByClassName("card__lastline__price")[0].innerText;
+    sobremesaPreco = sobremesaPreco.slice(3);
+    sobremesaPreco = sobremesaPreco.replace(",",".");
+    sobremesaPreco = parseFloat(sobremesaPreco);
+    let valorTotal = pratoPreco + bebidaPreco + sobremesaPreco;
+    valorTotal = valorTotal.toFixed(2);
+    let mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${pratoNome}\n\- Bebida: ${bebidaNome}\n\- Sobremesa: ${sobremesaNome}\n\- Total: R$ ${valorTotal}`;
+    mensagem = encodeURIComponent(mensagem);
+    let whatsappLink = `https://wa.me/5595981142025?text=${mensagem}`;
+    window.location.replace(whatsappLink);
+}
